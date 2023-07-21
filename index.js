b1adfcf5c1e596d6c32705cd87b164544e3b081c
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-21 11:40:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,63 +3,69 @@ const fs = require('fs');
 const Ajv = require('ajv');
 const ajvKeywords = require('ajv-keywords');
 
-
-const ajv = new Ajv.default({ allErrors: true });
+const ajv = new Ajv.default({ allErrors: true, allowUnionTypes: true });
 ajvKeywords(ajv, ['regexp']);
 
 async function main(_core) {
-  try {
-    // Read the JSON file path from the input
-    const jsonFilePath = _core.getInput('json-file');
-    const schemaFilePath = _core.getInput('schema-file');
+  try {
+    // Read the JSON file path from the input
+    const jsonFilePath = _core.getInput('json-file');
+    const schemaFilePath = _core.getInput('schema-file');
 
-    // Read the JSON file content
-    const jsonContent = fs.readFileSync(jsonFilePath, 'utf8');
-    
-      
-    // Read the schema file content
-    const schemaContent = fs.readFileSync(schemaFilePath, 'utf8');
-   
+    // Read the JSON file content
+    const jsonContent = fs.readFileSync(jsonFilePath, 'utf8');
 
-    // Parse JSON content into an object
-    const jsonData = JSON.parse(jsonContent);
-    
+    // Read the schema file content
+    const schemaContent = fs.readFileSync(schemaFilePath, 'utf8');
 
-    // Parse JSON schema content into an object
-    const jsonSchema = JSON.parse(schemaContent);
-//    delete schemaContent['$schema']
+    // Parse JSON content into an object
+    const jsonData = JSON.parse(jsonContent);
 
-    // Validate the JSON data against the schema
-    const isValid = validateRecipe(jsonData, jsonSchema);
+    // Parse JSON schema content into an object
+    const jsonSchema = JSON.parse(schemaContent);
+    delete jsonSchema['$schema']; // Remove $schema property from the schema
 
-    // Set the JSON content as an output
-    _core.setOutput('json', jsonContent);
+    // Validate the JSON data against the schema
+    const isValid = validateRecipe(jsonData, jsonSchema);
 
-    // Set the validation status as an output
-    _core.setOutput('isValid', isValid.toString()); // Convert boolean to string for output
-  } catch (error) {
-    _core.setFailed(`Action failed with error: ${error.message}`);
-  }
+    // Set the JSON content as an output
+    _core.setOutput('json', jsonContent);
+
+    // Set the validation status as an output
+    _core.setOutput('isValid', isValid.toString()); // Convert boolean to string for output
+
+    // If the recipe is valid, read and process the data
+    if (isValid) {
+      readRecipe(jsonData);
+    }
+  } catch (error) {
+    _core.setFailed(`Action failed with error: ${error.message}`);
+  }
 }
 
 function validateRecipe(data, schema) {
-  try {
-    const ajv = new Ajv.default({ allErrors: true });
-    ajvKeywords(ajv); // Call the imported function directly
-    const validate = ajv.compile(schema);
-    const isValid = validate(data);
+  try {
+    const validate = ajv.compile(schema);
+    const isValid = validate(data);
 
-    if (isValid) {
-      console.log('Recipe is valid!');
-      return true;
-    } else {
-      console.log('Recipe is invalid:', validate.errors);
-      return false;
-    }
-  } catch (error) {
-    console.log('Error occurred during validation:', error.message);
-    return false;
-  }
+    if (isValid) {
+      console.log('Recipe is valid!');
+      return true;
+    } else {
+      console.log('Recipe is invalid:', validate.errors);
+      return false;
+    }
+  } catch (error) {
+    console.log('Error occurred during validation:', error.message);
+    return false;
+  }
+}
+
+function readRecipe(data) {
+  const isValid = validateRecipe(data);
+  if (isValid) {
+    console.log('Substrate data:', data.substrate);
+  }
 }
 
 main(core);
