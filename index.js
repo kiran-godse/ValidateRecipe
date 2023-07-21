8a4c63177db75f7cc3a005f379e46a7aec37b907
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-21 08:45:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
 const core = require('@actions/core');
 const fs = require('fs');
 const Ajv = require('ajv');
+const ajvKeywords = require('ajv-keywords/keywords/regexp');
 
 async function main(_core) {
-  try {
-    // Read the JSON file path from the input
-    const jsonFilePath = _core.getInput('json-file');
-    const schemaFilePath = _core.getInput('schema-file');
+  try {
+    // Read the JSON file path from the input
+    const jsonFilePath = _core.getInput('json-file');
+    const schemaFilePath = _core.getInput('schema-file');
 
-    // Read the JSON file content
-    const jsonContent = fs.readFileSync(jsonFilePath, 'utf8');
+    // Read the JSON file content
+    const jsonContent = fs.readFileSync(jsonFilePath, 'utf8');
 
-    // Read the schema file content
-    const schemaContent = fs.readFileSync(schemaFilePath, 'utf8');
+    // Read the schema file content
+    const schemaContent = fs.readFileSync(schemaFilePath, 'utf8');
 
-    // Parse JSON content into an object
-    const jsonData = JSON.parse(jsonContent);
+    // Parse JSON content into an object
+    const jsonData = JSON.parse(jsonContent);
 
-    // Parse JSON schema content into an object
-    const jsonSchema = JSON.parse(schemaContent);
+    // Parse JSON schema content into an object
+    const jsonSchema = JSON.parse(schemaContent);
 
-    // Validate the JSON data against the schema
-    const isValid = validateRecipe(jsonData, jsonSchema);
+    // Validate the JSON data against the schema
+    const isValid = validateRecipe(jsonData, jsonSchema);
 
-    // Set the JSON content as an output
-    _core.setOutput('json', jsonContent);
+    // Set the JSON content as an output
+    _core.setOutput('json', jsonContent);
 
-    // Set the validation status as an output
-    _core.setOutput('isValid', isValid.toString()); // Convert boolean to string for output
-  } catch (error) {
-    _core.setFailed(`Action failed with error: ${error.message}`);
-  }
+    // Set the validation status as an output
+    _core.setOutput('isValid', isValid.toString()); // Convert boolean to string for output
+  } catch (error) {
+    _core.setFailed(`Action failed with error: ${error.message}`);
+  }
 }
 
 function validateRecipe(data, schema) {
-  try {
-    const ajv = new Ajv.default({ allErrors: true, keywords: ['regexp'] }); // Use built-in support for 'regexp' keyword
-    const validate = ajv.compile(schema);
-    const isValid = validate(data);
+  try {
+    const ajv = new Ajv.default({ allErrors: true });
+    ajvKeywords(ajv); // Call the imported function directly
+    const validate = ajv.compile(schema);
+    const isValid = validate(data);
 
-    if (isValid) {
-      console.log('Recipe is valid!');
-      return true;
-    } else {
-      console.log('Recipe is invalid:', validate.errors);
-      return false;
-    }
-  } catch (error) {
-    console.log('Error occurred during validation:', error.message);
-    return false;
-  }
+    if (isValid) {
+      console.log('Recipe is valid!');
+      return true;
+    } else {
+      console.log('Recipe is invalid:', validate.errors);
+      return false;
+    }
+  } catch (error) {
+    console.log('Error occurred during validation:', error.message);
+    return false;
+  }
 }
 
 main(core);
